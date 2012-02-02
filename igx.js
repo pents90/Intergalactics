c764d53ed70c0f699db898af913c2e41555e6f5d
John Watkinson <john@larvalabs.com>
2012-02-02 15:34:40 -0500

    Fleets and rudimentary invasions working.

--- igx.js
+++ igx.js
@@ -1,3 +1,6 @@
+var TURN_SPEED = 2000;
+var FLEET_SPEED = 0.2;
+
 var galaxy = [
 {name: 'A',
 ships: 20,
@@ -30,6 +33,18 @@ y: 11}
 
 console.log(galaxy);
 
+var turn = 0;
+var fleets = [
+{
+	source:1,
+	destination:0,
+	owner:0,
+	ships:30,
+	x:10,
+	y:2
+}
+];
+
 ////
 
 var fs = require('fs');
@@ -51,10 +66,50 @@ everyone.now.joinGame = function(msg){
 	everyone.now.receiveUpdate(galaxy);
 }
 
+everyone.now.sendFleet = function(fromPlanet, toPlanet, ships) {
+	// todo - check for fleet owner
+	var source = galaxy[fromPlanet];
+	var destination = galaxy[toPlanet];
+	var sent = Math.min(ships, source.ships);
+	fleets.push({source: fromPlanet, destination: toPlanet, owner: fromPlanet.owner, ships: sent, x:source.x, y:source.y});
+	source.ships -= sent;
+}
+
 setInterval(function() {
-	console.log("Update turn.");
-	for (var i = 0; i < galaxy.length; i++) {
-		galaxy[i].ships += galaxy[i].production;		 
+	turn++;
+	console.log("Update turn: " + turn + ".");
+	// Planets produce
+	if (turn % 20 == 0) {
+		for (var i = 0; i < galaxy.length; i++) {
+			galaxy[i].ships += galaxy[i].production;		 
+		}
+	}
+	// Fleets move
+	for (var i = 0; i < fleets.length; i++) {
+		var planet = galaxy[fleets[i].destination];
+		var distance = Math.sqrt((fleets[i].x - planet.x) ^ 2 + (fleets[i].y - planet.y) ^ 2);
+		if (distance < FLEET_SPEED) {
+			// Arrives
+			console.log("Fleet arrives!");
+			// Super-simple combat
+			if (fleets[i].owner == planet.owner) {
+				console.log("Planet reinforced.");
+				planet.ships += fleets[i].ships;
+			} else if (fleets[i].ships > planet.ships) {
+				console.log("Planet invaded.");
+				planet.owner = fleets[i].owner;
+				planet.ships = fleets[i].ships;
+			} else {
+				console.log("Fleet repelled.");				
+			}
+			fleets.splice(i, 1);
+			i--;
+		} else {
+			var angle = Math.atan2(planet.y - fleets[i].y, planet.x - fleets[i].x);
+			fleets[i].x += Math.cos(angle) * FLEET_SPEED;
+			fleets[i].y += Math.sin(angle) * FLEET_SPEED;
+			console.log("  Fleet " + i + " now at (" + fleets[i].x + ", " + fleets[i].y + ").")
+		}
 	}
 	everyone.now.receiveUpdate(galaxy);
-}, 2500);
+}, TURN_SPEED);
